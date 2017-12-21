beb38298bf8da577cf5397b30cacddd5b642a034
red <seokz20@gmail.com>
2017-12-21 11:17:41 +0900

    1초마다 반복하면서 데이터 저장하도록 구현.

--- index.js
+++ index.js
@@ -1,61 +1,98 @@
+// import { setTimeout } from 'timers';
+
 const express = require('express');
 const request = require('request');
-const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
-const app = express();
-
-const Coin = {
-    "BitCoin": 0,
-    "BitCoinCash": 1,
-    "Ethereum": 2,
-    "Ripple": 3,
-};
-
-var coins = [
-    {'name': '비트코인', 'price': '20000000', 'market_capitalization': '300조','updatedAt': Date()},
-    {'name': '비트코인캐시', 'price': '4200000', 'market_capitalization': '65조','updatedAt': Date()},
-]
-
-app.get('/coins', (req, res) => {
-    res.send(coins);
-});
-
-app.listen(3000, () => {
-    console.log('Server start on port 3000!.');
-    getAllInfo();
-});
-
-function getAllInfo() {
-    getCoinInfo(Coin.BitCoin);
-    getCoinInfo(Coin.BitCoinCash);
-    getCoinInfo(Coin.Ethereum);
-    getCoinInfo(Coin.Ripple);
-}
-
-function getCoinInfo(coin) {
-    const apiURL = "https://api.korbit.co.kr/v1/ticker";
-    var pair = "";
-    switch (coin) {
-        case Coin.BitCoin: {
-            pair = "?currency_pair=btc_krw";
-            break;
-        }
-        case Coin.BitCoinCash: {
-            pair = "?currency_pair=bch_krw";
-            break;
-        }
-        case Coin.Ethereum: {
-            pair = "?currency_pair=etc_krw";
-            break;
-        }
-        case Coin.Ripple: {
-            pair = "?currency_pair=xrp_krw";
-            break;
-        }
+const mongoUrl = `mongodb://localhost:27017/coins`
+MongoClient.connect(mongoUrl, (err, db) => {
+    const col = db.collection('currency_data')
+    if (err) {
+        console.log(err)
+        return
     }
-    request.get({ url: apiURL + pair}, (error, response, body) => { 
-        if (!error && response.statusCode == 200) { 
-            console.log(body);
+    const app = express();
+
+    const Coin = {
+        "BitCoin": "BitCoin",
+        "BitCoinCash": "BitCoinCash",
+        "Ethereum": "Ethereum",
+        "Ripple": "Ripple",
+    };
+
+    app.get('/coins', (req, res) => {
+        col.find({ updatedAt: { $gte: Date.now() - (1000 * 60 * 60 * 24) } }, { "_id": false, "name": true, "price": true, "updatedAt": true }).toArray().then(rows => {
+            // rows.forEach(row => console.log('', row))
+            res.json(rows);
+        })
+    });
+
+    app.listen(3000, () => {
+        console.log('Server start on port 3000!.');
+        getAllInfo();
+        // setInterval(getAllInfo, 1000);
+    });
+
+    function getAllInfo() {
+        Promise.all(
+            [
+                getCoinInfo(Coin.BitCoin),
+                getCoinInfo(Coin.BitCoinCash),
+                getCoinInfo(Coin.Ethereum),
+                getCoinInfo(Coin.Ripple),
+            ]
+        ).then(() => {
+            console.log('', 'Done');
+            setTimeout(getAllInfo, 1000);
+        }).catch(err => { 
+            console.error('Error: ', err);
+            setTimeout(getAllInfo, 10000); 
+        })
+
+    }
+
+    function getCoinInfo(coin) {
+        const apiURL = "https://api.korbit.co.kr/v1/ticker";
+        var pair = "";
+        switch (coin) {
+            case Coin.BitCoin: {
+                pair = "?currency_pair=btc_krw";
+                break;
+            }
+            case Coin.BitCoinCash: {
+                pair = "?currency_pair=bch_krw";
+                break;
+            }
+            case Coin.Ethereum: {
+                pair = "?currency_pair=etc_krw";
+                break;
+            }
+            case Coin.Ripple: {
+                pair = "?currency_pair=xrp_krw";
+                break;
+            }
         }
-    }); 
-}
+        return new Promise((resolve, reject) => {
+
+            request.get({ url: apiURL + pair }, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    body = JSON.parse(body)
+                    // console.log(body);
+
+                    const insObj = {
+                        name: coin,
+                        price: body.last,
+                        updatedAt: body.timestamp,
+                    }
+                    col.insert(insObj)//.then(r => console.log(r)).catch(e => console.log("error:", e))
+                        .then(() => resolve())
+                } else {
+                    // console.log("currency api error: " + error)
+                    console.log("status ", response.statusCode)
+                    reject('ERROR')
+                }
+            });
+        })
+    }
+
+})
